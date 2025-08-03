08e92201fce98fbb6d1ab6def431c5369b4b4940
Paulo Vilela <paulov4@hotmail.com>
2025-08-03 19:29:42 -0300

    Listagem, Remoção e edição de Estado

--- src/main/java/com/projeto/sistema_vendas/controller/EstadoController.java
+++ src/main/java/com/projeto/sistema_vendas/controller/EstadoController.java
@@ -3,11 +3,16 @@ package com.projeto.sistema_vendas.controller;
 import com.projeto.sistema_vendas.model.Estado;
 import com.projeto.sistema_vendas.repositories.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class EstadoController {
@@ -15,13 +20,40 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
+    //CRUD
+
+    //CADASTRAR
     @GetMapping("/cadastroEstado")
     public ModelAndView cadastrar(Estado estado){
-        ModelAndView mv = new ModelAndView("/estado/cadastro");
+        ModelAndView mv = new ModelAndView("administrativo/estado/cadastro");
         mv.addObject("estado", estado);
         return mv;
     }
 
+    //LISTAR
+    @GetMapping("/listarEstado")
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView("administrativo/estado/lista");
+        mv.addObject("listaEstados", estadoRepository.findAll());
+        return mv;
+    }
+
+    //EDITAR
+    @GetMapping("/editarEstado/{id}")
+    public ModelAndView editar(@PathVariable("id") Long id){
+        Optional<Estado> estado = estadoRepository.findById(id);
+        return cadastrar(estado.get());
+    }
+
+    //REMOVER
+    @GetMapping("/removerEstado/{id}")
+    public ModelAndView remover(@PathVariable("id") Long id){
+        Optional<Estado> estado = estadoRepository.findById(id);
+        estadoRepository.delete(estado.get());
+        return listar();
+    }
+
+    //SALVAR
     @PostMapping("/salvarEstado")
     public ModelAndView salvar(Estado estado, BindingResult result){
         if (result.hasErrors()){
